6f17fb1b10188ad0b85e03fb059ed859ed2adbe5
Philipp Ottlinger <ottlinger@365farmnet.com>
2016-01-26 20:00:50 +0100

    Issue #1: Use expectingExceptions rule and traditional way.

---
+++ src/test/java/de/aikiit/jmockex/ExpectingExceptionsTest.java
@@ -0,0 +1,51 @@
+/**
+ * Test Examples,  Copyright (C) 2016  P.Ottlinger
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.aikiit.jmockex;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.Matchers.containsString;
+
+/**
+ * https://github.com/junit-team/junit/wiki/Exception-testing
+ */
+public class ExpectingExceptionsTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test(expected = IllegalStateException.class)
+    public void theOldWay() {
+        new AnotherBean("example").perform();
+        // or try-catch - assertMessage
+    }
+
+    @Test
+    public void newWay() throws IllegalStateException {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("You are not allowed to perform anything here.");
+        thrown.expectMessage(containsString("You are not allowed to "));
+        // Enable to see nice error messages
+        // thrown.expect(hasProperty("name", is("example")));
+
+        // put your code at the end
+        new AnotherBean("example").perform();
+    }
+
+}
