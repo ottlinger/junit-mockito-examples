1ab9527daaa0bca6aabc978967913ca79c4ac515
Philipp Ottlinger <ottlinger@365farmnet.com>
2016-01-27 16:47:49 +0100

    Issue #2: LHF reduce warnings.

--- src/test/java/de/aikiit/jmockex/MockTests.java
+++ src/test/java/de/aikiit/jmockex/MockTests.java
@@ -29,7 +29,6 @@ import java.net.URLConnection;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
