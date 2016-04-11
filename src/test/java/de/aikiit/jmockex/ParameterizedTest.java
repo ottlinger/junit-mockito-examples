ab2d888818bc0fd0bda44bb9de9ed3a50b1cca0b
Hugo Hirsch <github@hugo-hirsch.de>
2016-04-11 21:52:37 +0200

    Add parameter list tests.

---
+++ src/test/java/de/aikiit/jmockex/ParameterizedTest.java
@@ -0,0 +1,53 @@
+/**
+ * Test Examples,  Copyright (C) 2016  P.Ottlinger
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.aikiit.jmockex;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ParameterizedTest {
+
+    @Parameterized.Parameters
+    public static List expectations() {
+        return Lists.newArrayList(new Object[][]{{1,12}, {2,13}});
+    }
+
+    private int number;
+    private int expectation;
+
+    public ParameterizedTest(int input, int  output) {
+        this.number = input;
+        this.expectation = output;
+    }
+
+    @Test
+    public void ensureCalculationIsCorrect() {
+        assertEquals(expectation, addElevenToIt(number));
+    }
+
+    public static int addElevenToIt(int number) {
+        return 11 + number;
+    }
+
+}
