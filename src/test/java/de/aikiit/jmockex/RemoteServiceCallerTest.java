647502979df6c237447c136cda090079993cf6cb
Hugo Hirsch <github@hugo-hirsch.de>
2016-06-18 20:46:43 +0200

    Improve test coverage.

--- src/test/java/de/aikiit/jmockex/RemoteServiceCallerTest.java
+++ src/test/java/de/aikiit/jmockex/RemoteServiceCallerTest.java
@@ -18,10 +18,10 @@ package de.aikiit.jmockex;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -30,13 +30,18 @@ public class RemoteServiceCallerTest {
     @Mock
     private RemoteServiceCaller.RemoteService remoteService;
 
-    @InjectMocks
     private RemoteServiceCaller caller = new RemoteServiceCaller();
 
     @Test
     public void argumentCaptor() {
         assertNotNull(caller);
         assertNotNull(remoteService);
+
+
+        final long timestamp = System.currentTimeMillis();
+        String converted = caller.serviceCall(timestamp);
+
+        assertEquals("alonglongway:"+timestamp, converted);
         // TODO to be continued :-D
 
     }
