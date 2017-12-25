ce17c824aaaf02c88509583e57c0b8d6a1ffbfe1
Hugo Hirsch <github@hugo-hirsch.de>
2017-12-25 22:08:57 +0100

    Issue #5: Add Mockito extension

--- src/test/java/de/aikiit/jmockex/TestingSystemOutPrintlnTest.java
+++ src/test/java/de/aikiit/jmockex/TestingSystemOutPrintlnTest.java
@@ -20,15 +20,17 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.contains;
 import static org.mockito.Matchers.startsWith;
 import static org.mockito.Mockito.verify;
+import org.junit.platform.runner.JUnitPlatform;
 
 import java.io.PrintStream;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
 public class TestingSystemOutPrintlnTest {
 
 	@Mock
