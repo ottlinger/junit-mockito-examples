d3af8ca9928ee163929de35e349bdad8c1787c8d
Hugo Hirsch <github@hugo-hirsch.de>
2024-07-20 22:06:42 +0200

    Fix javadoc warnings

--- src/main/java/de/aikiit/jmockex/OccurrenceFinder.java
+++ src/main/java/de/aikiit/jmockex/OccurrenceFinder.java
@@ -27,10 +27,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class OccurrenceFinder {
 
+        /**
+        * Checks if given marker is found in given source.
+        * @param source given source string to check in.
+        * @param marker marker String to find in source.
+        * @return {@code true} if marker is found in given source.
+        */
 	public static boolean containsExactlyOnce(String source, String marker) {
 		return containsExactlyTimes(1, source, marker);
 	}
 
+        /**
+        * Checks whether given marker is found n-times in given source.
+        * @param times how many times the match is found.
+        * @param source given source string to check in.
+        * @param marker marker String to find in source.
+        * @return {@code true} if marker is found in given source.
+        */
 	public static boolean containsExactlyTimes(int times, String source, String marker) {
 		if (Strings.isNullOrEmpty(source) || Strings.isNullOrEmpty(marker)) {
 			throw new IllegalArgumentException("No null arguments allowed");
